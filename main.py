cef95fa4e0b579a23d1c1b682ca96ab776bfb705
kalevagamer <kalevagamer@gmail.com>
2024-05-13 08:20:51 -0600

    first

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+def menu():
+  vtnMenu=Tk()
+  vtnMenu.title("Life Game")
+  vtnMenu.geometry("800x600")
+  vtnMenu.protocol("WM_DELETE_WINDOW",noCerrar())
+  vtnMenu.resizable(False,False)
+  vtnMenu.mainloop()
+
+def noCerrar():
+  pass
+
+menu()
